85b33068cbc2a158e33b19a1f770478043571e4e
CJw <waldc.astrion.us>
2026-05-04 16:21:43 -0400

    Added Latex Document

--- scripts/treeDir.sh
+++ scripts/treeDir.sh
@@ -1,6 +1,6 @@
-echo "This is the Wankel Engine Dir structure excluding all of the contents of the external library dirs" > DirStructure.txt
-tree -L 1 >> DirStructure.txt
-tree -L 1 external >> DirStructure.txt
-tree src >> DirStructure.txt
-tree SandboxApp >> DirStructure.txt
-tree scripts >> DirStructure.txt
+echo "This is the Wankel Engine Dir structure excluding all of the contents of the external library dirs" > Documents/DirStructure.txt
+tree -L 1 >> Documents/DirStructure.txt
+tree -L 1 external >> Documents/DirStructure.txt
+tree src >> Documents/DirStructure.txt
+tree SandboxApp >> Documents/DirStructure.txt
+tree scripts >> Documents/DirStructure.txt
