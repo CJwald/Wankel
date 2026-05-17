04e2360da2b3f391a4bc5403033d1f951554aa43
CJwald <cjwald888@gmail.com>
2026-05-17 09:22:16 -0400

    formatting

--- Sandbox/src/SandboxLayer.cpp
+++ Sandbox/src/SandboxLayer.cpp
@@ -46,8 +46,6 @@ SandboxLayer::SandboxLayer() : Layer("Cube"), m_Controller(1280.0f / 720.0f) {
 	m_ShipMesh = MeshLoader::Load("Assets/Mesh/SHIP04.ply");
 	m_GunMesh = MeshLoader::Load("Assets/Mesh/AK74_IRONS.ply");
 	m_BoxMesh = MeshLoader::Load("Assets/Mesh/Karachi.ply");
-	//m_BoxMesh = MeshLoader::Load("Assets/Mesh/Box.ply");
-	
 	m_CubeMesh = std::make_unique<Mesh>(Geometry::CubeVertices, sizeof(Geometry::CubeVertices), Geometry::CubeIndices, sizeof(Geometry::CubeIndices) / sizeof(uint32_t));
 
 	// Shader
@@ -93,7 +91,6 @@ SandboxLayer::SandboxLayer() : Layer("Cube"), m_Controller(1280.0f / 720.0f) {
     auto& follow = camEntity.AddComponent<FollowCameraComponent>();
 
     follow.Target = player;
-    //follow.Offset = {0.0f, 0.12f, 0.0f}; // I think i want to get this close to 0.0 and define mesh around that
     follow.Offset = {0.0f, 0.15f, 0.0f}; // I think i want to get this close to 0.0 and define mesh around that
 	float roll = 0.0f; float pitch = 0.0f; float yaw = 0.0f; 
 	follow.RotationOffset =
@@ -148,23 +145,10 @@ SandboxLayer::SandboxLayer() : Layer("Cube"), m_Controller(1280.0f / 720.0f) {
     auto& rb_box = b.AddComponent<RigidbodyComponent>();
     rb_box.IsStatic = true;
 	
-    //// WORLD BOX 2
-	//auto b2 = m_Scene.CreateEntity();
-    //auto& tb2 = b2.AddComponent<TransformComponent>();
-    //tb2.LocalPosition = {0.0f, 0.0f, 250.0f};
-    //tb2.LocalOrientation = 
-    //	glm::angleAxis(glm::radians(0.0f), glm::vec3(1,0,0)) *
-    //	glm::angleAxis(glm::radians(0.0f), glm::vec3(0,1,0)) *
-    //	glm::angleAxis(glm::radians(0.0f), glm::vec3(0,0,1));
-	//b2.AddComponent<MeshComponent>().MeshPtr = m_BoxMesh.get();
-    //auto& rb_box2 = b2.AddComponent<RigidbodyComponent>();
-    //rb_box2.IsStatic = true;
-
 
 	// DEFAULT FOG
-	//m_Fog.Color = {0.055f, 0.05f, 0.06f};
 	m_Fog.Color = {0.32f, 0.3f, 0.38f};
-	m_Fog.Density = 0.01f;
+	m_Fog.Density = 0.008f;
 
 	// Lock mouse initially
 	auto& window = Application::Get().GetWindow();
@@ -222,6 +206,7 @@ void SandboxLayer::OnUpdate() {
 	auto view = m_Scene.Registry().view<TransformComponent, MeshComponent>();
 
 	// X Y Z loop for tiling world
+	//  This needs to be factored out of this lol
 	for (int ix = -m_RepeatN; ix <= m_RepeatN; ix++) {
 	    for (int iy = -m_RepeatN; iy <= m_RepeatN; iy++) {
 	        for (int iz = -m_RepeatN; iz <= m_RepeatN; iz++) {
@@ -243,12 +228,8 @@ void SandboxLayer::OnUpdate() {
 
 				// Render
 				for (auto entity : view) {
-
 					auto& transform = view.get<TransformComponent>(entity);
     			    auto& mesh = view.get<MeshComponent>(entity);
-
-					//glm::mat4 model = transform.GetLocalTransform();
-					//glm::mat4 model = transform.WorldTransform;
 					glm::mat4 model = glm::translate(glm::mat4(1.0f), worldOffset) * transform.WorldTransform;
 
 					// PROCEDURAL MESH animATION
@@ -313,7 +294,6 @@ void SandboxLayer::OnImGuiRender() {
 	io.Fonts->GetGlyphRangesDefault();
 	if (!m_GameFocused) {
 		ImGui::Begin("Renderer Debug");
-
 		DebugOverlay::DrawFPSPanel();
 		
 		// FOG
@@ -325,65 +305,20 @@ void SandboxLayer::OnImGuiRender() {
 					ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_Float
 				);
 			}
-
-			ImGui::ColorEdit3(
-			    "Fog Color",
-			    &m_Fog.Color[0],
-				ImGuiColorEditFlags_Float
-			);
-
-			ImGui::SliderFloat(
-			    "Fog Density",
-			    &m_Fog.Density,
-			    0.0001f,
-			    0.1f,
-			    "%.4f",
-			    ImGuiSliderFlags_Logarithmic
-			);
+			ImGui::ColorEdit3("Fog Color", &m_Fog.Color[0], ImGuiColorEditFlags_Float);
+			ImGui::SliderFloat("Fog Density", &m_Fog.Density, 0.0001f, 0.1f, "%.4f", ImGuiSliderFlags_Logarithmic);
 
 			ImGui::Separator();
-			ImGui::Checkbox(
-			    "Noise Fog",
-			    &m_Fog.NoiseEnabled
-			);
-			
-			ImGui::SliderFloat(
-			    "Noise Scale",
-			    &m_Fog.NoiseScale,
-			    0.0001f,
-			    0.1f,
-			    "%.5f",
-			    ImGuiSliderFlags_Logarithmic
-			);
-			
-			ImGui::SliderFloat(
-			    "Noise Strength",
-			    &m_Fog.NoiseStrength,
-			    0.0f,
-			    4.0f
-			);
-			
-			ImGui::SliderInt(
-			    "Noise Octaves",
-			    &m_Fog.NoiseOctaves,
-			    1,
-			    8
-			);
+			ImGui::Checkbox("Noise Fog", &m_Fog.NoiseEnabled);
+			ImGui::SliderFloat("Noise Scale", &m_Fog.NoiseScale, 0.0001f, 0.1f, "%.5f", ImGuiSliderFlags_Logarithmic);
+			ImGui::SliderFloat("Noise Strength", &m_Fog.NoiseStrength, 0.0f, 4.0f);
+			ImGui::SliderInt("Noise Octaves", &m_Fog.NoiseOctaves, 1, 8);
 
 			ImGui::Separator();
 			ImGui::Text("Fog Wind");
-			ImGui::DragFloat3(
-			    "Wind Direction",
-			    &m_Fog.WindDir[0],
-			    0.01f
-			);
+			ImGui::DragFloat3("Wind Direction", &m_Fog.WindDir[0], 0.01f);
 			
-			ImGui::SliderFloat(
-			    "Wind Speed",
-			    &m_Fog.WindSpeed,
-			    0.0f,
-			    5.0f
-			);
+			ImGui::SliderFloat("Wind Speed", &m_Fog.WindSpeed, 0.0f, 5.0f);
 			
 			// normalize direction to avoid weird scaling
 			if (glm::length(m_Fog.WindDir) > 0.0001f)
@@ -422,54 +357,24 @@ void SandboxLayer::OnImGuiRender() {
 			
 			for (auto entity : animView) {
 			    auto& anim = animView.get<MeshAnimationComponent>(entity);
-			
 			    ImGui::Separator();
 			
 			    // POSITION AMPLITUDE
 			    ImGui::Text("Position");
 				glm::vec3 posAmpUI = anim.PositionAmplitude * 1000.0f;
-			    if (ImGui::DragFloat3(
-			        "Amplitude##Position",
-			        &posAmpUI[0],
-			        1.0f,
-			        0.0f,
-			        1000.0f
-			    	)) {
+			    if (ImGui::DragFloat3("Amplitude##Position", &posAmpUI[0], 1.0f, 0.0f, 1000.0f)) {
 					anim.PositionAmplitude = posAmpUI / 1000.0f;
 				}
 			
 			    // ROTATION AMPLITUDE
 			    ImGui::Text("Rotation");
-			    ImGui::DragFloat3(
-			        "Amplitude##Rotation",
-			        &anim.RotationAmplitude[0],
-			        0.1f,
-			        0.0f,
-			        50.0f
-			    );
+			    ImGui::DragFloat3("Amplitude##Rotation", &anim.RotationAmplitude[0], 0.1f, 0.0f, 50.0f);
 			
 			    ImGui::Separator();
 			    ImGui::Text("Position Tuning");
-			    ImGui::SliderFloat(
-			        "w - Frequency##Pos",
-			        &anim.PositionFrequency,
-			        0.1f,
-			        10.0f
-			    );
-			
-			    ImGui::SliderFloat(
-			        "z - Damping##Pos",
-			        &anim.PositionDamping,
-			        0.0f,
-			        3.0f
-			    );
-			
-			    ImGui::SliderFloat(
-			        "r - Response##Pos",
-			        &anim.PositionResponse,
-			        -3.0f,
-			        3.0f
-			    );
+			    ImGui::SliderFloat("w - Frequency##Pos", &anim.PositionFrequency, 0.1f, 10.0f);
+			    ImGui::SliderFloat("z - Damping##Pos", &anim.PositionDamping, 0.0f, 3.0f);
+			    ImGui::SliderFloat("r - Response##Pos", &anim.PositionResponse, -3.0f, 3.0f);
 
 				// Position Step Response
 				auto pvalues = Wankel::SecondOrderPreview::GetStepResponse(
@@ -477,39 +382,13 @@ void SandboxLayer::OnImGuiRender() {
 				    anim.PositionDamping,
 				    anim.PositionResponse
 				);
-				ImGui::PlotLines(
-				    "Step Response##Pos",
-				    pvalues.data(),
-				    (int)pvalues.size(),
-				    0,
-				    nullptr,
-				    -0.5f,
-				    2.0f,
-				    ImVec2(0, 100)
-				);
+				ImGui::PlotLines("Step Response##Pos", pvalues.data(), (int)pvalues.size(), 0, nullptr, -0.5f, 2.0f, ImVec2(0, 100));
 			
 			    ImGui::Separator();
 			    ImGui::Text("Rotation Tuning");
-			    ImGui::SliderFloat(
-			        "w - Frequency##Rot",
-			        &anim.RotationFrequency,
-			        0.1f,
-			        10.0f
-			    );
-			
-			    ImGui::SliderFloat(
-			        "z - Damping##Rot",
-			        &anim.RotationDamping,
-			        0.0f,
-			        3.0f
-			    ); //ζ
-			
-			    ImGui::SliderFloat(
-			        "r - Response##Rot",
-			        &anim.RotationResponse,
-			        -3.0f,
-			        3.0f
-			    );
+			    ImGui::SliderFloat("w - Frequency##Rot", &anim.RotationFrequency, 0.1f, 10.0f);
+			    ImGui::SliderFloat("z - Damping##Rot", &anim.RotationDamping, 0.0f, 3.0f); //ζ
+			    ImGui::SliderFloat("r - Response##Rot", &anim.RotationResponse, -3.0f, 3.0f);
 				
 				// Rotation Step Response
 				auto rvalues = Wankel::SecondOrderPreview::GetStepResponse(
@@ -517,17 +396,7 @@ void SandboxLayer::OnImGuiRender() {
 				    anim.RotationDamping,
 				    anim.RotationResponse
 				);
-				ImGui::PlotLines(
-				    "Step Response##Rot",
-				    rvalues.data(),
-				    (int)rvalues.size(),
-				    0,
-				    nullptr,
-				    -0.5f,
-				    2.0f,
-				    ImVec2(0, 100)
-				);
-			
+				ImGui::PlotLines("Step Response##Rot", rvalues.data(), (int)rvalues.size(), 0, nullptr, -0.5f, 2.0f, ImVec2(0, 100));
 			    break;
 			}
 		}
@@ -535,32 +404,12 @@ void SandboxLayer::OnImGuiRender() {
 		// WORLD DEBUG
 		if (ImGui::CollapsingHeader("World Tiling"))
 		{
-		    ImGui::SliderFloat(
-		        "Chunk Size",
-		        &m_ChunkSize,
-		        1.0f,
-		        500.0f,
-		        "%.1f"
-		    );
-		
-		    ImGui::SliderInt(
-		        "Repeat N",
-		        &m_RepeatN,
-		        0,
-		        10
-		    );
-		
-		    ImGui::Text("Grid: %d x %d x %d",
-		        2 * m_RepeatN + 1,
-		        2 * m_RepeatN + 1,
-		        2 * m_RepeatN + 1
-		    );
+		    ImGui::SliderFloat("Chunk Size", &m_ChunkSize, 1.0f, 500.0f, "%.1f");
+		    ImGui::SliderInt("Repeat N", &m_RepeatN, 0, 10);
+		    ImGui::Text("Grid: %d x %d x %d", 2 * m_RepeatN + 1, 2 * m_RepeatN + 1, 2 * m_RepeatN + 1);
 		}
 
-		ImGui::Checkbox(
-		    "Debug Draw",
-		    &Renderer::DebugEnabled
-		);
+		ImGui::Checkbox("Debug Draw", &Renderer::DebugEnabled);
 
 
 		// ENGINE INFO
