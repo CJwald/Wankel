4c8d175e05470c9e7076032abdb87420ccae1e46
CJ Wald <cjwald888@gmail.com>
2026-05-24 09:40:25 -0400

    Fixed some formatting

--- src/Wankel/Core/InputSystem.cpp
+++ src/Wankel/Core/InputSystem.cpp
@@ -8,8 +8,7 @@ namespace Wankel {
 
     static std::vector<SDL_Gamepad*> s_Gamepads;
 
-    void InputSystem::Init()
-    {
+    void InputSystem::Init() {
         if (SDL_Init(SDL_INIT_GAMEPAD | SDL_INIT_JOYSTICK | SDL_INIT_EVENTS) != 0) {
             WK_CORE_ERROR("SDL Init failed: {0}", SDL_GetError());
             return;
@@ -33,8 +32,7 @@ namespace Wankel {
         SDL_free(ids);
     }
 
-    void InputSystem::Shutdown()
-    {
+    void InputSystem::Shutdown() {
         for (auto* pad : s_Gamepads) {
             if (pad)
                 SDL_CloseGamepad(pad);
@@ -45,33 +43,26 @@ namespace Wankel {
         SDL_Quit();
     }
 
-    void InputSystem::PollControllers()
-    {
+    void InputSystem::PollControllers() {
         SDL_UpdateGamepads();
 
         SDL_Event e;
         while (SDL_PollEvent(&e)) {
 
-            switch (e.type)
-            {
-                case SDL_EVENT_GAMEPAD_ADDED:
-                {
+            switch (e.type) {
+                case SDL_EVENT_GAMEPAD_ADDED: {
                     SDL_Gamepad* pad = SDL_OpenGamepad(e.gdevice.which);
 
                     if (pad) {
                         s_Gamepads.push_back(pad);
 
-                        WK_CORE_INFO(
-                            "Controller connected: {0}",
-                            SDL_GetGamepadName(pad)
-                        );
+                        WK_CORE_INFO("Controller connected: {0}", SDL_GetGamepadName(pad));
                     }
 
                     break;
                 }
 
-                case SDL_EVENT_GAMEPAD_REMOVED:
-                {
+                case SDL_EVENT_GAMEPAD_REMOVED: {
                     WK_CORE_INFO("Controller disconnected");
                     // TODO: remove correct pad later
                     break;
@@ -86,27 +77,14 @@ namespace Wankel {
             if (!pad)
                 continue;
 
-            // =========================
             // AXES
-            // =========================
+            float lx = SDL_GetGamepadAxis(pad, SDL_GAMEPAD_AXIS_LEFTX) / 32767.0f;
+            float ly = SDL_GetGamepadAxis(pad, SDL_GAMEPAD_AXIS_LEFTY) / 32767.0f;
+            float rx = SDL_GetGamepadAxis(pad, SDL_GAMEPAD_AXIS_RIGHTX) / 32767.0f;
+            float ry = SDL_GetGamepadAxis(pad, SDL_GAMEPAD_AXIS_RIGHTY) / 32767.0f;
 
-            float lx =
-                SDL_GetGamepadAxis(pad, SDL_GAMEPAD_AXIS_LEFTX) / 32767.0f;
-
-            float ly =
-                SDL_GetGamepadAxis(pad, SDL_GAMEPAD_AXIS_LEFTY) / 32767.0f;
-
-            float rx =
-                SDL_GetGamepadAxis(pad, SDL_GAMEPAD_AXIS_RIGHTX) / 32767.0f;
-
-            float ry =
-                SDL_GetGamepadAxis(pad, SDL_GAMEPAD_AXIS_RIGHTY) / 32767.0f;
-
-            float l2 =
-                SDL_GetGamepadAxis(pad, SDL_GAMEPAD_AXIS_LEFT_TRIGGER) / 32767.0f;
-
-            float r2 =
-                SDL_GetGamepadAxis(pad, SDL_GAMEPAD_AXIS_RIGHT_TRIGGER) / 32767.0f;
+            float l2 = SDL_GetGamepadAxis(pad, SDL_GAMEPAD_AXIS_LEFT_TRIGGER) / 32767.0f;
+            float r2 = SDL_GetGamepadAxis(pad, SDL_GAMEPAD_AXIS_RIGHT_TRIGGER) / 32767.0f;
 
             ControllerInput::SetAxis((int)i, (int)GamepadAxis::LeftX, lx);
             ControllerInput::SetAxis((int)i, (int)GamepadAxis::LeftY, ly);
@@ -117,115 +95,40 @@ namespace Wankel {
             ControllerInput::SetAxis((int)i, (int)GamepadAxis::L2, l2);
             ControllerInput::SetAxis((int)i, (int)GamepadAxis::R2, r2);
 
-            // =========================
             // FACE BUTTONS
-            // =========================
+            ControllerInput::SetButton((int)i, (int)GamepadButton::Cross, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_SOUTH));
 
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::Cross,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_SOUTH)
-            );
+            ControllerInput::SetButton((int)i, (int)GamepadButton::Circle, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_EAST));
 
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::Circle,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_EAST)
-            );
+            ControllerInput::SetButton((int)i, (int)GamepadButton::Square, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_WEST));
 
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::Square,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_WEST)
-            );
+            ControllerInput::SetButton((int)i, (int)GamepadButton::Triangle, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_NORTH));
 
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::Triangle,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_NORTH)
-            );
-
-            // =========================
             // STICKS
-            // =========================
+            ControllerInput::SetButton((int)i, (int)GamepadButton::L3, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_LEFT_STICK));
 
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::L3,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_LEFT_STICK)
-            );
+            ControllerInput::SetButton((int)i, (int)GamepadButton::R3, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_RIGHT_STICK));
 
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::R3,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_RIGHT_STICK)
-            );
-
-            // =========================
             // SHOULDERS
-            // =========================
+            ControllerInput::SetButton((int)i, (int)GamepadButton::L1, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_LEFT_SHOULDER));
 
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::L1,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_LEFT_SHOULDER)
-            );
+            ControllerInput::SetButton((int)i, (int)GamepadButton::R1, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_RIGHT_SHOULDER));
 
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::R1,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_RIGHT_SHOULDER)
-            );
-
-            // =========================
             // CENTER BUTTONS
-            // =========================
+            ControllerInput::SetButton((int)i, (int)GamepadButton::Back, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_BACK));
 
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::Back,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_BACK)
-            );
+            ControllerInput::SetButton((int)i, (int)GamepadButton::Start, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_START));
 
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::Start,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_START)
-            );
+            ControllerInput::SetButton((int)i, (int)GamepadButton::PS, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_GUIDE));
 
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::PS,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_GUIDE)
-            );
-
-            // =========================
             // DPAD
-            // =========================
+            ControllerInput::SetButton((int)i, (int)GamepadButton::DPadUp, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_DPAD_UP));
 
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::DPadUp,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_DPAD_UP)
-            );
+            ControllerInput::SetButton((int)i, (int)GamepadButton::DPadDown, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_DPAD_DOWN));
 
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::DPadDown,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_DPAD_DOWN)
-            );
+            ControllerInput::SetButton((int)i, (int)GamepadButton::DPadLeft, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_DPAD_LEFT));
 
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::DPadLeft,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_DPAD_LEFT)
-            );
-
-            ControllerInput::SetButton(
-                (int)i,
-                (int)GamepadButton::DPadRight,
-                SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_DPAD_RIGHT)
-            );
+            ControllerInput::SetButton((int)i, (int)GamepadButton::DPadRight, SDL_GetGamepadButton(pad, SDL_GAMEPAD_BUTTON_DPAD_RIGHT));
         }
     }
 
