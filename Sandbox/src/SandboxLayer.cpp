bc43803c8f8516a5aa75ccc5d99ba6f3719eb4c9
CJ Wald <cjwald888@gmail.com>
2026-05-24 11:47:50 -0400

    ImGui always on

--- Sandbox/src/SandboxLayer.cpp
+++ Sandbox/src/SandboxLayer.cpp
@@ -594,7 +594,7 @@ void SandboxLayer::OnUpdate() {
 void SandboxLayer::OnImGuiRender() {
 	ImGuiIO& io = ImGui::GetIO();
 	io.Fonts->GetGlyphRangesDefault();
-	if (!m_GameFocused) {
+	if (!m_GameFocused || m_GameFocused) {// making this always on now. Better tesing
 		ImGui::Begin("Renderer Debug");
 		DebugOverlay::DrawFPSPanel();
 		
